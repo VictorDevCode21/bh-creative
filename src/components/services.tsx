846542c752749037bab400176f1a95f423d5d5f8
Victor <victorenrique2002@gmail.com>
2023-09-25 23:11:13 -0400

    services-updated

--- src/components/services.tsx
+++ src/components/services.tsx
@@ -2,65 +2,54 @@
 
 function Services() {
     return (
-        <section id="services" className="w-full h-full ">
+        <section id="services" className="w-full lg:h-screen xs:h-full">
             <div className="w-full flex items-center xs:justify-start md:justify-start xs:pt-28 xs:pb-5 md:pt-4 md:pb-0 md:pl-40 lg:pt-0">
                 <p className="font-bold text-3xl text-cyan-400 lg:pt-20 xs:pl-4 md:pl-0">Servicios <br></br> en los que nos especializamos</p>
             </div>
-            <div className="w-full flex md:flex-row xs:flex-col items-end justify-start md:pt-6 md:pr-0 lg:pl-36">
-                <div className="md:w-60 xs:pl-4 xs:pr-4 md:pl-0 pr-4 lg:pl-4">
-                    <div className="w-full h-40 border-t-2 border-blue-500 ">
-                        <h3 className="text-lg font-semibold xs:pb-4 xs:pt-4 md:pb-0 lg:pt-2 lg:pb-4">Diseños</h3>
-                        <p className="text-sm">Nuestros diseñadores ayudan a hacer logotipos para empresas nuevas o rehacer antiguos para
-                            volverlos más llamativos dando una imagen más profesional y que el público guste de ella. Otro
-                            servicio que realizamos es el de fabricar miniaturas para canales de Youtube.
+            <div className="w-full flex md:flex-row xs:flex-col items-end justify-center md:pt-6 md:pr-0 lg:pl-36 lg:pr-40">
+                <div className="lg:w-1/2 md:w-60 xs:pl-4 xs:pr-4 md:pl-0  lg:pl-8 ">
+                    <div className="w-full lg:h-40 xs:h-full border-t-2 border-gray-500">
+                        <h3 className="text-lg font-bold xs:pb-4 xs:pt-4 md:pb-0 lg:pt-2 lg:pb-4 ">Desarrollo de Páginas web</h3>
+                        <p className="xs:text-sm lg:text-base">
+                            En BH Creative, creamos experiencias web desde cero, utilizando las últimas y más innovadoras tecnologías
+                            de programación. Nuestro enfoque no solo se centra en la estética, sino también en la funcionalidad y la
+                            visibilidad en línea.
                         </p>
+                        <div>
+                            <p className="lg:text-lg lg:pt-4 lg:font-medium xs:font-medium xs:pt-2">Tipos de páginas web que ofrecemos:</p>
+                            <h2 className="lg:text-base lg:pt-2 xs:pt-4">
+                                Páginas Web Estáticas: Potenciamos tu presencia en línea al proporcionar información general y formas de contacto de manera clara y efectiva.
+                            </h2>
+                            <h2 className="lg:text-base lg:pt-2 xs:pt-4">
+                                Páginas Web Dinámicas: Fomentamos la interacción y la participación del usuario a través de soluciones web dinámicas y atractivas.
+                            </h2>
+                            <h2 className="lg:text-base lg:pt-2 xs:pt-4">
+                                E-Commerce Personalizado: Impulsa tus ventas en línea con nuestra plataforma de comercio electrónico. No solo creamos tiendas en línea, sino que también optimizamos cada aspecto para proporcionar una experiencia de compra inolvidable.
+                            </h2>
+                            <p className="lg:text-base lg:pt-2 xs:pt-4">
+                                Además, nuestra dedicación al SEO garantiza que tu sitio web alcance las primeras posiciones en los resultados de búsqueda de Google, asegurando que tu audiencia te encuentre con facilidad.
+                            </p>
+                        </div>
                     </div>
                 </div>
-                <div className="md:w-60 xs:pl-4 xs:pr-4 md:pl-0  lg:pl-8">
-                    <div className="w-full h-40 border-t-2 border-blue-500">
-                        <h3 className="text-lg font-semibold xs:pb-4 xs:pt-4 md:pb-0 lg:pt-2 lg:pb-4 ">Páginas web</h3>
-                        <p className="text-sm">Las páginas que realizamos elevan el nivel de tráfico(vistas recibidas) usando palabras claves
-                            para los buscadores y creandolas con las últimas tecnologías como lo son Nextjs, TypeScript y
-                            Prisma.
-                        </p>
-                    </div>
-                </div>
-                <div className="md:w-60 xs:pl-4 xs:pr-4 md:pl-4 lg:pl-8">
-                    <div className="w-full h-40 border-t-2 border-blue-500">
-                        <h3 className="text-lg font-semibold xs:pb-4 xs:pt-4 md:pb-0 lg:pt-2 lg:pb-4">Copywriting</h3>
-                        <p className="text-sm">En el servicio de Copywriting nos enfocamos en aplicar estrategias de persuasión que inciten a los
-                            consumidores a darle clicks a tus anuncios y leer los emails. Creando así una tasa alta de
-                            respuesta y ventas mediante estos dos medios.
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div className="flex md:flex-row xs:flex-col items-end justify-end md:pt-16 md:pr-0 lg:pr-0 lg:pt-24">
-                <div className="md:w-1/6 xs:pl-4 xs:pr-4 md:pl-0 pr-4 lg:pl-4">
-                    <div className="w-full h-40 border-t-2 border-blue-500 ">
-                        <h3 className=" text-lg font-semibold xs:pb-4 xs:pt-2 md:pb-0 lg:pt-2 lg:pb-4">Community manager</h3>
-                        <p className=" text-sm">
-                            Impulsamos tu presencia en línea con estrategias de Community Management
-                            diseñadas a medida para conectar con tu audiencia de manera auténtica y efectiva.
-                        </p>
-                    </div>
-                </div>
-                <div className="md:w-1/6 xs:pl-4 xs:pr-4 md:pl-0 lg:pl-4">
-                    <div className="w-full h-40 border-t-2 border-blue-500 ">
-                        <h3 className="text-lg font-semibold xs:pb-4 xs:pt-4 md:pb-0 lg:pt-2 lg:pb-4">Edición de videos</h3>
-                        <p className="text-sm">Especialistas en reels para Instagram o videos de Tiktok simples, pero tampoco nos quedamos
-                            atrás editando videos largos para Youtube e Instagram.
-                        </p>
-                    </div>
-                </div>
-                <div className="md:w-1/6 xs:pl-4 xs:pr-4 md:pl-0 lg:pl-4 ">
-                    <div className="w-full h-40 border-t-2 border-blue-500 ">
-                        <h3 className="text-lg font-semibold xs:pb-4 xs:pt-4 md:pb-0 lg:pt-2 lg:pb-4">Diseño web</h3>
-                        <p className="text-sm">
-                            Tenemos los mejores diseñadores de páginas web que se encargan de hacer que tu página sea
-                            llamativa y atractiva para el público. Además de que son 100% personalizables y adaptables
-                            al estilo e imagen de tu marca.
-                        </p>
+                <div className="lg:w-1/2 md:w-60 xs:pl-4 xs:pr-4 md:pl-4 lg:pt-0 lg:pb-0 lg:pl-8 xs:pt-10 ">
+                    <div className="w-full lg:h-40 xs:h-full border-t-2 border-gray-500 ">
+                        <h3 className="text-lg font-bold xs:pb-4 xs:pt-4 md:pb-0 lg:pt-2 lg:pb-4">Automatización y optimización de tu negocio en redes</h3>
+                        <div>
+                            <p className="lg:text-base lg:pt-2 xs:pt-2 ">
+                                En BH Creative, no solo creamos estrategias de marketing digital,
+                                sino que las personalizamos para cumplir con tus objetivos y
+                                metas únicas. ¿Buscas aumentar la conciencia de tu marca,
+                                impulsar el tráfico a tu sitio web o mejorar las conversiones? ¡Estamos aquí para ti!
+                            </p>
+                            <p className="lg:text-base lg:pt-2 xs:pt-6 ">
+                                Nuestro equipo de expertos domina la gestión y optimización de perfiles en plataformas líderes como Facebook, Twitter, Instagram, LinkedIn y Tiktok.
+                            </p>
+                            <p className="lg:text-base lg:pt-2 xs:pt-6 ">
+                                Creamos contenido cautivador, ejecutamos campañas publicitarias altamente enfocadas y monitorizamos el rendimiento en tiempo real para asegurarnos
+                                de que tu presencia en redes sociales destaque.
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
